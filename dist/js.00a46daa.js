150de0c637c9746a30f7f0a2edfa166bb271fcbd
MagnumGoYB <sosbs@outlook.com>
2021-06-18 14:49:06 +0800

    :rocket: feat: deploy gh-pages

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -129,7 +129,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire
 })(
   {
-    TNS6: [
+    '../node_modules/gsap/gsap-core.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -5452,7 +5452,7 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
       },
       {}
     ],
-    bp4Z: [
+    '../node_modules/gsap/CSSPlugin.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -7446,9 +7446,9 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
 
         _gsapCore.gsap.registerPlugin(CSSPlugin)
       },
-      { './gsap-core.js': 'TNS6' }
+      { './gsap-core.js': '../node_modules/gsap/gsap-core.js' }
     ],
-    TpQl: [
+    '../node_modules/gsap/index.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -7602,9 +7602,12 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
         exports.TweenMax = TweenMaxWithCSS
         exports.default = exports.gsap = gsapWithCSS
       },
-      { './gsap-core.js': 'TNS6', './CSSPlugin.js': 'bp4Z' }
+      {
+        './gsap-core.js': '../node_modules/gsap/gsap-core.js',
+        './CSSPlugin.js': '../node_modules/gsap/CSSPlugin.js'
+      }
     ],
-    MgTz: [
+    'js/utils.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -7767,7 +7770,7 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
       },
       {}
     ],
-    LMRJ: [
+    'js/cursor.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -7909,9 +7912,9 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
 
         exports.default = Cursor
       },
-      { gsap: 'TpQl', './utils': 'MgTz' }
+      { gsap: '../node_modules/gsap/index.js', './utils': 'js/utils.js' }
     ],
-    cxbS: [
+    'js/content-page.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -7945,7 +7948,7 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
       },
       {}
     ],
-    evi4: [
+    'js/menu-item.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -8092,9 +8095,12 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
 
         exports.default = MenuItem
       },
-      { gsap: 'TpQl', './content-page': 'cxbS' }
+      {
+        gsap: '../node_modules/gsap/index.js',
+        './content-page': 'js/content-page.js'
+      }
     ],
-    ECw1: [
+    'js/menu-controller.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -8721,9 +8727,13 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
 
         exports.default = MenuController
       },
-      { './utils': 'MgTz', gsap: 'TpQl', './menu-item': 'evi4' }
+      {
+        './utils': 'js/utils.js',
+        gsap: '../node_modules/gsap/index.js',
+        './menu-item': 'js/menu-item.js'
+      }
     ],
-    QvaY: [
+    'js/index.js': [
       function (require, module, exports) {
         'use strict'
 
@@ -8752,10 +8762,249 @@ _totalTimeToTime = (clampedTotalTime, duration, repeat, repeatDelay, yoyo) => {
           })
         })
       },
-      { './cursor': 'LMRJ', './menu-controller': 'ECw1' }
+      {
+        './cursor': 'js/cursor.js',
+        './menu-controller': 'js/menu-controller.js'
+      }
+    ],
+    '../node_modules/parcel-bundler/src/builtins/hmr-runtime.js': [
+      function (require, module, exports) {
+        var global = arguments[3]
+        var OVERLAY_ID = '__parcel__error__overlay__'
+        var OldModule = module.bundle.Module
+
+        function Module(moduleName) {
+          OldModule.call(this, moduleName)
+          this.hot = {
+            data: module.bundle.hotData,
+            _acceptCallbacks: [],
+            _disposeCallbacks: [],
+            accept: function (fn) {
+              this._acceptCallbacks.push(fn || function () {})
+            },
+            dispose: function (fn) {
+              this._disposeCallbacks.push(fn)
+            }
+          }
+          module.bundle.hotData = null
+        }
+
+        module.bundle.Module = Module
+        var checkedAssets, assetsToAccept
+        var parent = module.bundle.parent
+
+        if (
+          (!parent || !parent.isParcelRequire) &&
+          typeof WebSocket !== 'undefined'
+        ) {
+          var hostname = '' || location.hostname
+          var protocol = location.protocol === 'https:' ? 'wss' : 'ws'
+          var ws = new WebSocket(
+            protocol + '://' + hostname + ':' + '57229' + '/'
+          )
+
+          ws.onmessage = function (event) {
+            checkedAssets = {}
+            assetsToAccept = []
+            var data = JSON.parse(event.data)
+
+            if (data.type === 'update') {
+              var handled = false
+              data.assets.forEach(function (asset) {
+                if (!asset.isNew) {
+                  var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id)
+
+                  if (didAccept) {
+                    handled = true
+                  }
+                }
+              }) // Enable HMR for CSS by default.
+
+              handled =
+                handled ||
+                data.assets.every(function (asset) {
+                  return asset.type === 'css' && asset.generated.js
+                })
+
+              if (handled) {
+                console.clear()
+                data.assets.forEach(function (asset) {
+                  hmrApply(global.parcelRequire, asset)
+                })
+                assetsToAccept.forEach(function (v) {
+                  hmrAcceptRun(v[0], v[1])
+                })
+              } else if (location.reload) {
+                // `location` global exists in a web worker context but lacks `.reload()` function.
+                location.reload()
+              }
+            }
+
+            if (data.type === 'reload') {
+              ws.close()
+
+              ws.onclose = function () {
+                location.reload()
+              }
+            }
+
+            if (data.type === 'error-resolved') {
+              console.log('[parcel] ✨ Error resolved')
+              removeErrorOverlay()
+            }
+
+            if (data.type === 'error') {
+              console.error(
+                '[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack
+              )
+              removeErrorOverlay()
+              var overlay = createErrorOverlay(data)
+              document.body.appendChild(overlay)
+            }
+          }
+        }
+
+        function removeErrorOverlay() {
+          var overlay = document.getElementById(OVERLAY_ID)
+
+          if (overlay) {
+            overlay.remove()
+          }
+        }
+
+        function createErrorOverlay(data) {
+          var overlay = document.createElement('div')
+          overlay.id = OVERLAY_ID // html encode message and stack trace
+
+          var message = document.createElement('div')
+          var stackTrace = document.createElement('pre')
+          message.innerText = data.error.message
+          stackTrace.innerText = data.error.stack
+          overlay.innerHTML =
+            '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' +
+            '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' +
+            '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' +
+            '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' +
+            message.innerHTML +
+            '</div>' +
+            '<pre>' +
+            stackTrace.innerHTML +
+            '</pre>' +
+            '</div>'
+          return overlay
+        }
+
+        function getParents(bundle, id) {
+          var modules = bundle.modules
+
+          if (!modules) {
+            return []
+          }
+
+          var parents = []
+          var k, d, dep
+
+          for (k in modules) {
+            for (d in modules[k][1]) {
+              dep = modules[k][1][d]
+
+              if (
+                dep === id ||
+                (Array.isArray(dep) && dep[dep.length - 1] === id)
+              ) {
+                parents.push(k)
+              }
+            }
+          }
+
+          if (bundle.parent) {
+            parents = parents.concat(getParents(bundle.parent, id))
+          }
+
+          return parents
+        }
+
+        function hmrApply(bundle, asset) {
+          var modules = bundle.modules
+
+          if (!modules) {
+            return
+          }
+
+          if (modules[asset.id] || !bundle.parent) {
+            var fn = new Function(
+              'require',
+              'module',
+              'exports',
+              asset.generated.js
+            )
+            asset.isNew = !modules[asset.id]
+            modules[asset.id] = [fn, asset.deps]
+          } else if (bundle.parent) {
+            hmrApply(bundle.parent, asset)
+          }
+        }
+
+        function hmrAcceptCheck(bundle, id) {
+          var modules = bundle.modules
+
+          if (!modules) {
+            return
+          }
+
+          if (!modules[id] && bundle.parent) {
+            return hmrAcceptCheck(bundle.parent, id)
+          }
+
+          if (checkedAssets[id]) {
+            return
+          }
+
+          checkedAssets[id] = true
+          var cached = bundle.cache[id]
+          assetsToAccept.push([bundle, id])
+
+          if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+            return true
+          }
+
+          return getParents(global.parcelRequire, id).some(function (id) {
+            return hmrAcceptCheck(global.parcelRequire, id)
+          })
+        }
+
+        function hmrAcceptRun(bundle, id) {
+          var cached = bundle.cache[id]
+          bundle.hotData = {}
+
+          if (cached) {
+            cached.hot.data = bundle.hotData
+          }
+
+          if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+            cached.hot._disposeCallbacks.forEach(function (cb) {
+              cb(bundle.hotData)
+            })
+          }
+
+          delete bundle.cache[id]
+          bundle(id)
+          cached = bundle.cache[id]
+
+          if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+            cached.hot._acceptCallbacks.forEach(function (cb) {
+              cb()
+            })
+
+            return true
+          }
+        }
+      },
+      {}
     ]
   },
   {},
-  ['QvaY'],
+  ['../node_modules/parcel-bundler/src/builtins/hmr-runtime.js', 'js/index.js'],
   null
 )
+//# sourceMappingURL=/js.00a46daa.js.map
